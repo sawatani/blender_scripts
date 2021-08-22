2c5a52586c4f19d2c3a6b477e4c0b489d2625397
沢谷 邦夫 <devel@fathens.org>
2021-08-22 17:38:35 +0900

    トゲトゲの作成

--- jigzag.py
+++ jigzag.py
@@ -4,9 +4,11 @@
 X軸を固定した平面として、横と縦に Z, Y 軸とする。
 複数の面がY方向に連なっている状態を前提としている。
 """
+import math
 
-import bpy
 import bmesh
+import bpy
+from mathutils import Vector
 
 
 def get_grouped_vertices(vts):
@@ -34,19 +36,56 @@ def get_vertices_edge(vts):
     raise ValueError(f"No connection found: {vts}")
 
 
+class VertYZ:
+    """
+    複数の頂点を Z座標とY座標で並べ替えた配列。
+    ２次元配列にして、１次元目がY座標、２次元目がZ座標になる。
+    """
+
+    def __init__(self, lists_by_yz):
+        self.lists = lists_by_yz
+
+    def get_size_y(self):
+        return len(self.lists)
+
+    def get_size_z(self):
+        return len(self.lists[0])
+
+    def get(self, y, z):
+        return self.lists[y][z]
+
+    def get_next_y(self, y, z):
+        if 0 < y < len(self.lists) - 1:
+            return [self.lists[y - 1][z], self.lists[y + 1][z]]
+        else:
+            return None
+
+    def get_left_bottom(self, face_y, face_z):
+        return self.lists[face_y][face_z]
+
+    def get_left_top(self, face_y, face_z):
+        return self.lists[face_y][face_z + 1]
+
+    def get_right_bottom(self, face_y, face_z):
+        return self.lists[face_y + 1][face_z]
+
+    def get_right_top(self, face_y, face_z):
+        return self.lists[face_y + 1][face_z + 1]
+
+
 def sort_vertices_zy(vts):
     """
-    複数の頂点を Z座標とY座標で並べ替える。
-    ２次元配列にして、１次元目がY座標、２次元目がZ座標になる。
+    複数の頂点を Z座標とY座標で並べ替え VertYZ として返す。
     """
     grouped = get_grouped_vertices(vts)
     grouped.sort(key=lambda ys: ys[0].co.y)
-    return list(map(lambda ys: sorted(ys, key=lambda v: v.co.z), grouped))
+    lists = list(map(lambda ys: sorted(ys, key=lambda v: v.co.z), grouped))
+    return VertYZ(lists)
 
 
 def create_verts(bm, ncuts):
     """
-    ncuts の数だけZ方向に辺を分割し、全ての頂点を２次元配列にまとめる。
+    ncuts の数だけZ方向に辺を分割し、全ての頂点を VertYZ にまとめる。
     """
     yss = get_grouped_vertices(bm.verts)
     edges = sorted(map(get_vertices_edge, yss), key=lambda e: e.verts[0].co.y)
@@ -55,17 +94,75 @@ def create_verts(bm, ncuts):
     return sort_vertices_zy(bm.verts)
 
 
-def create_triangle_faces(yzs):
-    pass
+def create_triangle_faces(yzs: VertYZ):
+    """
+    VertYZ の頂点でジグザグに面を作成する。
+    """
+    bpy.ops.mesh.select_mode(type="VERT")
+
+    def mk_face(*verts):
+        bpy.ops.mesh.select_all(action="DESELECT")
+        for v in verts:
+            v.select = True
+        bpy.ops.mesh.vert_connect()
+
+    for iy in range(yzs.get_size_y() - 1):
+        for iz in range(yzs.get_size_z() - 1):
+            if (iy + iz) % 2 == 0:
+                base = [
+                    yzs.get_left_top(iy, iz),
+                    yzs.get_right_bottom(iy, iz),
+                ]
+                mk_face(yzs.get_left_bottom(iy, iz), *base)
+                mk_face(yzs.get_right_top(iy, iz), *base)
+            else:
+                base = [
+                    yzs.get_right_top(iy, iz),
+                    yzs.get_left_bottom(iy, iz),
+                ]
+                mk_face(yzs.get_right_bottom(iy, iz), *base)
+                mk_face(yzs.get_left_top(iy, iz), *base)
+
+
+def make_togetoge(yzs: VertYZ, rate):
+    """
+    VertYZ の頂点をトゲトゲになるようにX,Y軸の法線方向に動かす。
+    動かす大きさは {両隣の頂点の距離} * rate で指定される。
+    """
+    ny = yzs.get_size_y()
+    nz = yzs.get_size_z()
+
+    def get_vector(vert1, vert2):
+        dx = vert1.co[0] - vert2.co[0]
+        dy = vert1.co[1] - vert2.co[1]
+        t = -dx / dy
+        s = math.sqrt((dx ** 2 + dy ** 2) / (t ** 2 + 1))
+        return Vector([s, s * t, 0]) * rate
+
+    def move_vertex(y, z):
+        vert = yzs.get(y, z)
+        nexts = yzs.get_next_y(y, z)
+        if nexts:
+            vert.co += get_vector(*nexts)
+
+    def in_range(start):
+        for iy in range(start, yzs.get_size_y(), 2):
+            for iz in range(start, yzs.get_size_z(), 2):
+                move_vertex(iy, iz)
+
+    in_range(0)
+    in_range(1)
+
 
 # Start
 obj = bpy.context.active_object
 print(f"Starting with: {obj}")
-bpy.ops.object.mode_set(mode='EDIT')
+bpy.ops.object.mode_set(mode="EDIT")
 bm = bmesh.from_edit_mesh(obj.data)
 
 yzs = create_verts(bm, 3)
 create_triangle_faces(yzs)
+make_togetoge(yzs, 0.5)
 
 # Finish
 bpy.context.view_layer.update()
